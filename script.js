273222d02f3b20d5214adab627c4efee3158e630
Ana Carolina Bizinoto <acbsnutri@gmail.com>
2022-06-09 10:30:14 -0300

    update CSS

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ const imagens = [
     '4.png',
     '5.png',
     '6.png',
-    'card.png'
 ];
 
 let cardHTML = '';
 
 imagens.forEach(img => {
     cardHTML += `
-        <div>
-            <img src="img/${img}">
-            <img src="img/card.png">
+        <div class="cartao-memoria">
+            <img class="cartao-frente" src="img/${img}">
+            <img class="cartao-costas" src="img/card.png">
         </div>
     `
 })
 
-tabuleiro.innerHTML = cardHTML;
+tabuleiro.innerHTML = cardHTML + cardHTML;
